034f7f0da0d955189dd67bfbacc60593d2bed422
Nick Pitt <ngpitt@xphysics.net>
2017-12-18 11:57:21 -0500

    Updated client-go

--- load-simulator.go
+++ load-simulator.go
@@ -7,9 +7,9 @@ import (
 	"sync"
 	"time"
 
+	"k8s.io/api/autoscaling/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/pkg/apis/autoscaling/v1"
 	"k8s.io/client-go/rest"
 )
 
